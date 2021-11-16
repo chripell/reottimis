1e74f24d8d611b03e0966572745ca72a703f224a
Christian Pellegrin <chipell@gmail.com>
2021-11-16 15:48:30 +0000

    Improved formatting in generated HTML.

--- reottimis_lapse.py
+++ reottimis_lapse.py
@@ -99,8 +99,8 @@ def generate_index(cfg: ConfigParser):
                 continue
             fd.write("".join((
                 '<p><a href="', v, '">',
-                m[1], "-", m[2], "-", m[3], " ", m[4], ":", m[5], "-",
-                m[6], "-", m[7], "-", m[8], " ", m[9], ":", m[10], "-",
+                m[1], "-", m[2], "-", m[3], " ", m[4], ":", m[5], " - ",
+                m[6], "-", m[7], "-", m[8], " ", m[9], ":", m[10],
                 "</a></p>\n")))
         fd.write("</body></html>\n")
 
