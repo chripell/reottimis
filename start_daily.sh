b63cc37492672dbc3ffcfc1376a4ff3425dbb8c7
Christian <chripell@gmail.com>
2026-02-13 08:27:21 +0000

    Fixed daily cleanup for new site.

--- start_daily.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. /home/chri/python-virtual-environments/rpi/bin/activate
-
-cd /mnt/d/chri/reottimis
-exec nice /mnt/d/chri/reottimis/reottimis_daily.py 'yesterday' /mnt/d/chri/reolink.cfg
-
-
-
